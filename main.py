9216b2fe2f4e6edd61f8264879d76217498865e8
aqueleleee <116666891+aqueleleee@users.noreply.github.com>
2022-11-03 12:10:37 +0600

    Update main.py

--- main.py
+++ main.py
@@ -8,33 +8,40 @@ app = QApplication([])
 win = QWidget()
 win.resize(400,200)
 win.setWindowTitle('memory card')
-text = QLabel('какой национальности не существует?')
+text = QLabel()
 ans = QPushButton('ответить')
 
 
 text1 = QLabel('правильно/неправильно')
 text2 = QLabel('правильный ответ')
 
+
 class Question():
-    def __init__(self,q,ans1,ans2,ans3,ans4):
+    def __init__(self, q, ans1, wr2, wr3, wr4):
         self.q = q
         self.ans1 = ans1
-        self.ans2 = ans2
-        self.ans3 = ans3
-        self.ans4 = ans4
-        answers = [ans1,ans2,ans3,ans4]
-        shuffle(answers)
-        self.answers = answers
+        self.wr2 = wr2
+        self.wr3 = wr3
+        self.wr4 = wr4
 
-q1 = Question('день независимости кыргызыстана','31 августа','7 апреля','8 марта','31 декабря')
 
-text.setText(q1.q)
+
+quest1 = Question('день независимости кыргызыстана', '31 августа', '7 апреля', '8 марта', '31 декабря')
+quest2 = Question('Сколько синих полос на флаге США?',0,7,8,4)
+quest3 = Question('Сколько костей в теле человека?',206,280,170,649)
+quest4 = Question('Как назывался корабль капитана Джека Воробья в "Пиратах Карибского моря"?',
+                  'Черная жемчужина','Мародер','Слизерин','Черный питон')
+
+qlist = [quest1,quest2,quest3,quest4]
 
 groupbox = QGroupBox('варианты ответов')
-a1 = QRadioButton(q1.answers[0])
-a2 = QRadioButton(q1.answers[1])
-a3 = QRadioButton(q1.answers[2])
-a4 = QRadioButton(q1.answers[3])
+a1 = QRadioButton()
+a2 = QRadioButton()
+a3 = QRadioButton()
+a4 = QRadioButton()
+
+answers = [a1,a2,a3,a4]
+
 
 button = QPushButton('ответить')
 
@@ -45,17 +52,17 @@ buttonbox.addButton(a3)
 buttonbox.addButton(a4)
 
 def checkans():
-    if a1.isChecked():
+    if answers[0].isChecked():
         text1.setText('правильно')
-        return a1.text()
+        return answers[0].text()
     else:
         text1.setText('неправильно')
-        if a2.isChecked():
-            return a2.text()
-        if a3.isChecked():
-            return a3.text()
-        if a4.isChecked():
-            return a4.text()
+        if answers[1].isChecked():
+            return answers[1].text()
+        if answers[2].isChecked():
+            return answers[2].text()
+        if answers[3].isChecked():
+            return answers[3].text()
 
 
 def cleanbut():
@@ -66,6 +73,12 @@ def cleanbut():
     a4.setChecked(False)
     buttonbox.setExclusive(True)
 
+def question():
+    cleanbut()
+    nextq()
+    groupbox1.hide()
+    groupbox.show()
+    button.setText('ответить')
 
 def ans():
     checkans()
@@ -76,12 +89,7 @@ def ans():
     groupbox1.show()
     button.setText('следующий вопрос')
 
-def question():
-    cleanbut()
-    text.setText('какой национальности не существует?')
-    groupbox1.hide()
-    groupbox.show()
-    button.setText('ответить')
+
 
 
 def checkbutton():
@@ -90,12 +98,6 @@ def checkbutton():
     elif button.text() == 'следующий вопрос':
         question()
 
-def ask(q,*args):
-    text.setText(q)
-    a1.setText(args[1])
-    a3.setText(args[2])
-    a3.setText(args[3])
-    a4.setText(args[4])
 
 button.clicked.connect(checkbutton)
 
@@ -134,6 +136,24 @@ layt2.addWidget(groupbox)
 thelayt.addLayout(layt1)
 thelayt.addLayout(layt2)
 thelayt.addLayout(laytbut)
+
+
+
+def nextq():
+    time += 1
+    if time >= len(qlist):
+        time = 0
+    addq(qlist[time])
+
+time = -1
+
+def addq(cls):
+    shuffle(answers)
+    text.setText(cls.q)
+    answers[0].setText(cls.ans1)
+    answers[1].setText(cls.wr2)
+    answers[2].setText(cls.wr3)
+    answers[3].setText(cls.wr4)
 
 
 win.setLayout(thelayt)
